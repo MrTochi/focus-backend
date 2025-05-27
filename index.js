6cc25daa715ff605edf742bdece2d00b34d21943
MrTochi <okonkwotochuks@gmail.com>
2025-05-27 08:34:15 +0100

    first commit2

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://focus-frontend-duma.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
